66934b23b0c87094e79fb99e619c1db77361eca6
Derek Page <america971@gmail.com>
2019-05-08 23:01:17 -0500

    Adding AutoMove

---
+++ AutoMove/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace AutoMove
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._btnStartPolling = new System.Windows.Forms.Button();
+            this._txtWatchDir = new System.Windows.Forms.TextBox();
+            this._txtMoveDir = new System.Windows.Forms.TextBox();
+            this._btnSelectMoveDir = new System.Windows.Forms.Button();
+            this._btnSelectWatchDir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this._nudPollInterval = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this._optRename = new System.Windows.Forms.RadioButton();
+            this._optOverwrite = new System.Windows.Forms.RadioButton();
+            this._chkDeleteSourceFile = new System.Windows.Forms.CheckBox();
+            this._pgbProgress = new System.Windows.Forms.ProgressBar();
+            this._chkOnlyMoveIfChanged = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this._nudPollInterval)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _btnStartPolling
+            // 
+            this._btnStartPolling.Location = new System.Drawing.Point(50, 339);
+            this._btnStartPolling.Name = "_btnStartPolling";
+            this._btnStartPolling.Size = new System.Drawing.Size(188, 40);
+            this._btnStartPolling.TabIndex = 0;
+            this._btnStartPolling.Text = "Start Polling";
+            this._btnStartPolling.UseVisualStyleBackColor = true;
+            this._btnStartPolling.Click += new System.EventHandler(this._btnStartPolling_Click);
+            // 
+            // _txtWatchDir
+            // 
+            this._txtWatchDir.Location = new System.Drawing.Point(49, 29);
+            this._txtWatchDir.Name = "_txtWatchDir";
+            this._txtWatchDir.Size = new System.Drawing.Size(220, 22);
+            this._txtWatchDir.TabIndex = 1;
+            this._txtWatchDir.Text = "D:\\eMule0.50a\\incoming";
+            this._txtWatchDir.TextChanged += new System.EventHandler(this._txtWatchDir_TextChanged);
+            // 
+            // _txtMoveDir
+            // 
+            this._txtMoveDir.Location = new System.Drawing.Point(49, 86);
+            this._txtMoveDir.Name = "_txtMoveDir";
+            this._txtMoveDir.Size = new System.Drawing.Size(220, 22);
+            this._txtMoveDir.TabIndex = 2;
+            this._txtMoveDir.Text = "D:\\Moved";
+            this._txtMoveDir.TextChanged += new System.EventHandler(this._txtMoveDir_TextChanged);
+            // 
+            // _btnSelectMoveDir
+            // 
+            this._btnSelectMoveDir.Location = new System.Drawing.Point(15, 86);
+            this._btnSelectMoveDir.Name = "_btnSelectMoveDir";
+            this._btnSelectMoveDir.Size = new System.Drawing.Size(28, 23);
+            this._btnSelectMoveDir.TabIndex = 3;
+            this._btnSelectMoveDir.Text = "...";
+            this._btnSelectMoveDir.UseVisualStyleBackColor = true;
+            this._btnSelectMoveDir.Click += new System.EventHandler(this._btnSelectMoveDir_Click);
+            // 
+            // _btnSelectWatchDir
+            // 
+            this._btnSelectWatchDir.Location = new System.Drawing.Point(15, 29);
+            this._btnSelectWatchDir.Name = "_btnSelectWatchDir";
+            this._btnSelectWatchDir.Size = new System.Drawing.Size(28, 23);
+            this._btnSelectWatchDir.TabIndex = 3;
+            this._btnSelectWatchDir.Text = "...";
+            this._btnSelectWatchDir.UseVisualStyleBackColor = true;
+            this._btnSelectWatchDir.Click += new System.EventHandler(this._btnSelectWatchDir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Move Files To Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Watch Directory For Files";
+            // 
+            // _txtOutput
+            // 
+            this._txtOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this._txtOutput.Location = new System.Drawing.Point(0, 398);
+            this._txtOutput.Multiline = true;
+            this._txtOutput.Name = "_txtOutput";
+            this._txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this._txtOutput.Size = new System.Drawing.Size(308, 108);
+            this._txtOutput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Poll Interval (ms)";
+            // 
+            // _nudPollInterval
+            // 
+            this._nudPollInterval.Location = new System.Drawing.Point(165, 306);
+            this._nudPollInterval.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this._nudPollInterval.Name = "_nudPollInterval";
+            this._nudPollInterval.Size = new System.Drawing.Size(71, 22);
+            this._nudPollInterval.TabIndex = 7;
+            this._nudPollInterval.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this._nudPollInterval.ValueChanged += new System.EventHandler(this._nudPollInterval_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this._optRename);
+            this.groupBox1.Controls.Add(this._optOverwrite);
+            this.groupBox1.Location = new System.Drawing.Point(70, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(146, 79);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conflict Resolution";
+            // 
+            // _optRename
+            // 
+            this._optRename.AutoSize = true;
+            this._optRename.Checked = true;
+            this._optRename.Location = new System.Drawing.Point(6, 48);
+            this._optRename.Name = "_optRename";
+            this._optRename.Size = new System.Drawing.Size(82, 21);
+            this._optRename.TabIndex = 10;
+            this._optRename.TabStop = true;
+            this._optRename.Text = "Rename";
+            this._optRename.UseVisualStyleBackColor = true;
+            // 
+            // _optOverwrite
+            // 
+            this._optOverwrite.AutoSize = true;
+            this._optOverwrite.Location = new System.Drawing.Point(6, 21);
+            this._optOverwrite.Name = "_optOverwrite";
+            this._optOverwrite.Size = new System.Drawing.Size(89, 21);
+            this._optOverwrite.TabIndex = 9;
+            this._optOverwrite.Text = "Overwrite";
+            this._optOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // _chkDeleteSourceFile
+            // 
+            this._chkDeleteSourceFile.AutoSize = true;
+            this._chkDeleteSourceFile.Checked = true;
+            this._chkDeleteSourceFile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this._chkDeleteSourceFile.Location = new System.Drawing.Point(50, 247);
+            this._chkDeleteSourceFile.Name = "_chkDeleteSourceFile";
+            this._chkDeleteSourceFile.Size = new System.Drawing.Size(146, 21);
+            this._chkDeleteSourceFile.TabIndex = 9;
+            this._chkDeleteSourceFile.Text = "Delete Source File";
+            this._chkDeleteSourceFile.UseVisualStyleBackColor = true;
+            // 
+            // _pgbProgress
+            // 
+            this._pgbProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this._pgbProgress.Location = new System.Drawing.Point(0, 506);
+            this._pgbProgress.Name = "_pgbProgress";
+            this._pgbProgress.Size = new System.Drawing.Size(308, 23);
+            this._pgbProgress.TabIndex = 10;
+            // 
+            // _chkOnlyMoveIfChanged
+            // 
+            this._chkOnlyMoveIfChanged.AutoSize = true;
+            this._chkOnlyMoveIfChanged.Checked = true;
+            this._chkOnlyMoveIfChanged.CheckState = System.Windows.Forms.CheckState.Checked;
+            this._chkOnlyMoveIfChanged.Location = new System.Drawing.Point(50, 265);
+            this._chkOnlyMoveIfChanged.Name = "_chkOnlyMoveIfChanged";
+            this._chkOnlyMoveIfChanged.Size = new System.Drawing.Size(222, 21);
+            this._chkOnlyMoveIfChanged.TabIndex = 9;
+            this._chkOnlyMoveIfChanged.Text = "Only Copy if File Has Changed";
+            this._chkOnlyMoveIfChanged.UseVisualStyleBackColor = true;
+            this._chkOnlyMoveIfChanged.CheckedChanged += new System.EventHandler(this._chkMoveChanged_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 529);
+            this.Controls.Add(this._chkOnlyMoveIfChanged);
+            this.Controls.Add(this._chkDeleteSourceFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this._nudPollInterval);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._btnSelectWatchDir);
+            this.Controls.Add(this._btnSelectMoveDir);
+            this.Controls.Add(this._txtMoveDir);
+            this.Controls.Add(this._txtWatchDir);
+            this.Controls.Add(this._btnStartPolling);
+            this.Controls.Add(this._pgbProgress);
+            this.Name = "frmMain";
+            this.Text = "Auto Move (Automatic File Mover)";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._nudPollInterval)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btnStartPolling;
+        private System.Windows.Forms.TextBox _txtWatchDir;
+        private System.Windows.Forms.TextBox _txtMoveDir;
+        private System.Windows.Forms.Button _btnSelectMoveDir;
+        private System.Windows.Forms.Button _btnSelectWatchDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown _nudPollInterval;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton _optRename;
+        private System.Windows.Forms.RadioButton _optOverwrite;
+        private System.Windows.Forms.CheckBox _chkDeleteSourceFile;
+        private System.Windows.Forms.ProgressBar _pgbProgress;
+        private System.Windows.Forms.CheckBox _chkOnlyMoveIfChanged;
+    }
+}
+
---
+++ AutoMove/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace AutoMove
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        System.Windows.Threading.DispatcherTimer _tmrFilePoller;
+
+        private void NotifyCreateDir(string dir)
+        {
+            //Show dialog to user to create a directory
+
+            DialogResult dialogResult;
+
+            dialogResult = System.Windows.Forms.MessageBox.Show(
+                "Create Directory?",
+                "The directory " + _txtMoveDir.Text + " not exist, would you like to create it?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+
+                    System.IO.Directory.CreateDirectory(dir);
+                }
+                catch (Exception ex)
+                {
+                    Log("Couldn not create directory " + dir + "\r\n" + ex.ToString());
+                }
+            }
+        }
+
+        private string ValidatePoll()
+        {
+            //Returns String.Empty if validation succeeded
+            string strMsg = "";
+
+
+            if (!System.IO.Directory.Exists(_txtMoveDir.Text))
+                strMsg += "Directory " + _txtMoveDir.Text + " does not exist.";
+            if (!System.IO.Directory.Exists(_txtWatchDir.Text))
+                strMsg += "Directory " + _txtWatchDir.Text + " does not exist.";
+
+            return strMsg;
+        }
+        private void _btnStartPolling_Click(object sender, EventArgs e)
+        {
+            if (_tmrFilePoller != null && _tmrFilePoller.IsEnabled)
+            {
+                EndPoll();
+            }
+            else
+            {
+                string res = ValidatePoll();
+                if (res == String.Empty)
+                    BeginPoll();
+                else
+                {
+                    Log("Polling could not start. \r\n " + res);
+                }
+            }
+        }
+
+        private void BeginPoll()
+        {
+            _tmrFilePoller = new System.Windows.Threading.DispatcherTimer();
+            _tmrFilePoller.Tick += new EventHandler(_tmrFilePoller_Tick);
+            _tmrFilePoller.Interval = new TimeSpan(0, 0, 0, 0, (int)_nudPollInterval.Value);
+            _tmrFilePoller.Start();
+            _btnStartPolling.Text = "Stop";
+            _txtMoveDir.Enabled = false;
+            _txtWatchDir.Enabled = false;
+        }
+        private void EndPoll()
+        {
+            _tmrFilePoller.Stop();
+            _txtMoveDir.Enabled = true;
+            _txtWatchDir.Enabled = true;
+            _btnStartPolling.Text = "Start";
+        }
+
+        private void _tmrFilePoller_Tick(object sender, EventArgs e)
+        {
+            MoveFiles();
+        }
+        private void MoveFiles()
+        {
+            string[] files = System.IO.Directory.GetFiles(_txtWatchDir.Text);
+            Log("Moving " + files.Length + " files...");
+            _pgbProgress.Value = 0;
+            for (int iFile = 0; iFile < files.Length; iFile++)
+            {
+                string filePath = files[iFile];
+
+                try
+                {
+                    string srcName, dstName;
+                    srcName = filePath;
+                    dstName = System.IO.Path.Combine(_txtMoveDir.Text, System.IO.Path.GetFileName(filePath));
+
+                    //Skip moving unchanged files.
+                    if (System.IO.File.Exists(dstName))
+                    {
+                        if (_chkOnlyMoveIfChanged.Checked)
+                        {
+                            DateTime ftime = System.IO.File.GetLastWriteTime(srcName);
+                            DateTime ftime2 = System.IO.File.GetLastWriteTime(dstName);
+                            if (ftime == ftime2)
+                                continue;
+                        }
+
+                        //Rename file if specified.
+                        if (_optRename.Checked)
+                        {
+                            dstName = RenameExistingFile(dstName);
+                        }
+                        else if (_optOverwrite.Checked)
+                        {
+                            //Do nothing.
+                        }
+                        else
+                            throw new Exception("Error: No option selected for handling duplicate files.");
+                    }
+
+                    System.IO.File.Copy(srcName, dstName);
+                    Log("Copied " + srcName + " to " + dstName);
+                    if (_chkDeleteSourceFile.Checked)
+                    {
+                        System.IO.File.Delete(srcName);
+                        Log("Deleted " + srcName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log("Error: Could not move file " + filePath + "\r\n" + ex.ToString());
+                }
+
+                _pgbProgress.Value = (int)(((double)iFile / (double)files.Length) * 100.0);
+            }
+            _pgbProgress.Value = 0;
+
+        }
+        int iLine = 0;
+        private void Log(string msg)
+        {
+            _txtOutput.AppendText("[" + iLine.ToString() + "] " + msg + "\r\n");
+            _txtOutput.SelectionStart = _txtOutput.Text.Length + 1; // add some logic if length is 0
+            iLine++;
+        }
+
+        private void _btnSelectWatchDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.ShowDialog();
+            _txtWatchDir.Text = fbd.SelectedPath;
+        }
+
+        private void _btnSelectMoveDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.ShowDialog();
+            _txtMoveDir.Text = fbd.SelectedPath;
+        }
+
+        private void _txtWatchDir_TextChanged(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(_txtWatchDir.Text) && _txtWatchDir.Text.Length > 0)
+                NotifyCreateDir(_txtWatchDir.Text);
+        }
+
+        private void _txtMoveDir_TextChanged(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(_txtMoveDir.Text) && _txtMoveDir.Text.Length > 0)
+                NotifyCreateDir(_txtMoveDir.Text);
+        }
+        private string RenameExistingFile(string dstName)
+        {
+            string newName = "";
+            for (int i = 0; i < 100; ++i)
+            {
+                string path = System.IO.Path.GetDirectoryName(dstName);
+                string fn = System.IO.Path.GetFileNameWithoutExtension(dstName);
+                string ext = System.IO.Path.GetExtension(dstName);
+                newName = System.IO.Path.Combine(path, fn) + "_" + i.ToString() + ext;
+                if (!System.IO.File.Exists(newName))
+                    break;
+            }
+            if (System.IO.File.Exists(newName))
+                throw new Exception("Failed to copy " + dstName + " File could not be renamed.");
+            else
+                return newName;
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _nudPollInterval_ValueChanged(object sender, EventArgs e)
+        {
+            EndPoll();
+            BeginPoll();
+        }
+
+        private void _chkMoveChanged_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
